6855d81820b66dab737f727fe1aa374fec465f22
machine <analyticrye@gmail.com>
2020-03-18 21:10:30 -0400

    started basic stuff

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerTurn: true
+    };
+  }
+  onHandleAttack = () => {
+    this.setState({
+      playerTurn: !this.state.playerTurn
+    });
+  };
+  render() {
+    if (this.state.playerTurn) {
+      return (
+        <div>
+          <button onClick={this.onHandleAttack}>Hit</button>
+          <div>Player's turn</div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.onHandleAttack}>Continue</button>
+          <div>Monster's turn</div>
+        </div>
+      );
+    }
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector("#root"));
