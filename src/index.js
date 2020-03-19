46e8fe94c2ffb4d7153a482892f4af7f901fa9db
machine <analyticrye@gmail.com>
2020-03-19 14:35:51 -0400

    minor refactor for state

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      playerTurn: true
-    };
-  }
+  state = {
+    playerTurn: true
+  };
   onHandleAttack = () => {
     this.setState({
       playerTurn: !this.state.playerTurn
